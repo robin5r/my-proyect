285a770abd7cdc0f7ce1f6a728c6e6dabbfadc75
robin5r <robinrvilelallatas@gmail.com>
2025-05-16 11:15:03 -0500

    sitio con paginas, enlaces y formularios

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,27 @@
 import HeaderComponent from "../components/HeaderComponent"
 
 function BlogPage() {
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    console.log("El Formulario se ha enviado")
+  }
   return (
    <>
    <HeaderComponent></HeaderComponent>
-   <div>BlogPage</div>
+   <div>BlogPage
+    <h1>Formulario - comenta</h1>
+   <form onSubmit={handlesubmit}>
+    <fieldset>
+      <label htmlFor="username">Nombre</label>
+      <input type="text" id="username"/>
+    </fieldset>
+    <fieldset>
+      <label htmlFor="comentario">Comentario</label>
+      <input type="text" id="comentario" />
+    </fieldset>
+    <button>Enviar</button>
+   </form>
+   </div>
    </>
     
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,19 @@ import HeaderComponent from "../components/HeaderComponent"
 
 
 function HomePage() {
+  const saySomething = () =>{
+    console.log("Di Algo");
+    alert("Ahora");
+  }
   return (
     <>
       <HeaderComponent></HeaderComponent>
       <div>HomePage</div>
+      <hr/>
+      <button onClick={saySomething}>Pulsa Click</button>
     </>
     
   )
-}
+} 
 
 export default HomePage
